134406027a2c95eead55d22671540b1cfd5ab9cc
Rishita <154823062+RishitaCS@users.noreply.github.com>
2025-07-13 02:11:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@
 # app.py
 
 import streamlit as st
-from retriever import DocumentRetriever
-from generator import AnswerGenerator
+from Retriever import DocumentRetriever
+from Generator import AnswerGenerator
 import tempfile
 import os
 import PyPDF2
